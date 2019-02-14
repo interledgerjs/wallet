24a36fb9c4de4ecc420172a3beaaf4248482ff5a
revan-wy <revan-wy@student.wethinkcode.co.za>
2019-02-14 14:16:42 +0200

    clean up of status.ts

--- src/controllers/status.ts
+++ src/controllers/status.ts
@@ -1,6 +1,4 @@
 import { Request, Response } from 'express'
-//import * as mysql from 'mysql';
-//import bodyParser = require('body-parser');
 import * as dbFunctions from "../db";
 import * as Joi from "joi";
 import * as jwt from "jsonwebtoken";
@@ -27,8 +25,6 @@ export let get_acc_by_account_id = (req: Request, res: Response) => {
 }
 
 export let create_new_acc = (req: Request, res: Response) => {
-    //let sql = `INSERT INTO accounts (account_name, owner_user_id, balance, last_updated) VALUES ('${req.body.account_name}', '${req.body.owner_user_id}', '${req.body.balance}', DEFAULT)`;
-    //let values = ['${req.body.account_name}', '${req.body.owner_user_id}', '${req.body.balance}'];
     dbFunctions.query(`INSERT INTO accounts (account_name, owner_user_id, balance, last_updated) VALUES ('${req.body.account_name}', '${req.body.owner_user_id}', '${req.body.balance}', DEFAULT)`, (err, result) => {
         if (err) 
             res.status(500).json(err)
@@ -41,7 +37,6 @@ export let create_new_acc = (req: Request, res: Response) => {
 //              implement simultaneous updating of dbt and crdt acc's
 export let update_acc = (req: Request, res: Response) => {
     //get old balance
-    //let sql1 = `SELECT * FROM accounts WHERE account_id = '${req.body.account_id}'`;
     dbFunctions.query(`SELECT * FROM accounts WHERE account_id = '${req.body.account_id}'`, (err, result) => {
         if (err) 
             res.status(500).json(err)
@@ -49,7 +44,6 @@ export let update_acc = (req: Request, res: Response) => {
             //calc new balance
             var new_balance = result[0].balance + req.body.amount;
             //update with new balance
-            //let sql = `UPDATE accounts SET balance = '${new_balance}', last_updated = DEFAULT WHERE account_id = '${req.body.account_id}'`;
             dbFunctions.query(`UPDATE accounts SET balance = '${new_balance}', last_updated = DEFAULT WHERE account_id = '${req.body.account_id}'`, (err, result) => {
                 if (err) 
                     res.status(500).json(err)
@@ -59,41 +53,6 @@ export let update_acc = (req: Request, res: Response) => {
         } 
     })
 }
-
-// unused block of transaction functions
-/*export let get_trans_by_acc_id = (req: Request, res: Response) => {
-    let sql = "SELECT * FROM transactions WHERE dbt_acc_id = " + req.params.acc_id + " OR crdt_acc_id = " + req.params.acc_id;
-    connection.query(sql, (err, result) => {
-        if (err) 
-            res.status(500).json(err)
-        else
-            res.json(result)
-    })
-}
-
-export let get_trans_by_trans_id = (req: Request, res: Response) => {
-    let sql = "SELECT * FROM transactions WHERE trans_id = " + req.params.trans_id;
-    connection.query(sql, (err, result) => {
-        if (err) 
-            res.status(500).json(err)
-        else
-            res.json(result)
-    })
-}
-
-//FUTURE WORK: include sql query to update accounts table
-export let create_new_trans = (req: Request, res: Response) => {
-    let trans = req.body;
-    //run debit on accounts table
-    //run credit on accounts table
-    let sql = "INSERT INTO transactions SET ?"
-    connection.query(sql, trans, (err, result) => {
-        if (err) 
-            res.status(500).json(err)
-        else
-            res.json(result)
-    })
-}*/
 
 export let get_user_by_user_id = (req: Request, res: Response) => {
     let sql = "SELECT * FROM users WHERE user_id = " + req.params.user_id;
