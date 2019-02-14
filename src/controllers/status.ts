1213ded491c54462dc3470f47ffac295100048b4
revan-wy <revan-wy@student.wethinkcode.co.za>
2019-02-14 12:59:39 +0200

    imported users and accounts modules

--- src/controllers/status.ts
+++ src/controllers/status.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express'
-import * as mysql from 'mysql';
-import bodyParser = require('body-parser');
+//import * as mysql from 'mysql';
+//import bodyParser = require('body-parser');
 import * as dbFunctions from "../db";
 import * as Joi from "joi";
 import * as jwt from "jsonwebtoken";
@@ -8,8 +8,7 @@ import * as jwt from "jsonwebtoken";
 
 
 export let get_acc_by_owner_user_id = (req: Request, res: Response) => {
-    let sql: string = "SELECT * FROM accounts WHERE owner_user_id = ?";
-    dbFunctions.queryRet(`SELECT * FROM accounts WHERE owner_user_id = '${req.params.id}'`, (result) => {
+    dbFunctions.query(`SELECT * FROM accounts WHERE owner_user_id = '${req.params.id}'`, (err, result) => {
         if (result.length == 0) 
             res.status(500).json(err)
         else
@@ -18,8 +17,8 @@ export let get_acc_by_owner_user_id = (req: Request, res: Response) => {
 }
 
 export let get_acc_by_account_id = (req: Request, res: Response) => {
-    let sql = "SELECT * FROM accounts WHERE account_id = ?";
-    connection.query(sql, req.params.account_id, (err, result) => {
+    let sql = `SELECT * FROM accounts WHERE account_id = ?`;
+    dbFunctions.query(`SELECT * FROM accounts WHERE account_id = '${req.params.account_id}'`, (err, result) => {
         if (err) 
             res.status(500).json(err)
         else
@@ -28,9 +27,9 @@ export let get_acc_by_account_id = (req: Request, res: Response) => {
 }
 
 export let create_new_acc = (req: Request, res: Response) => {
-    let sql = "INSERT INTO accounts (account_name, owner_user_id, balance, last_updated) VALUES (?, ?, ?, DEFAULT)";
-    let values = [req.body.account_name, req.body.owner_user_id, req.body.balance];
-    connection.query(sql, values, (err, result) => {
+    //let sql = `INSERT INTO accounts (account_name, owner_user_id, balance, last_updated) VALUES ('${req.body.account_name}', '${req.body.owner_user_id}', '${req.body.balance}', DEFAULT)`;
+    //let values = ['${req.body.account_name}', '${req.body.owner_user_id}', '${req.body.balance}'];
+    dbFunctions.query(`INSERT INTO accounts (account_name, owner_user_id, balance, last_updated) VALUES ('${req.body.account_name}', '${req.body.owner_user_id}', '${req.body.balance}', DEFAULT)`, (err, result) => {
         if (err) 
             res.status(500).json(err)
         else
@@ -42,16 +41,16 @@ export let create_new_acc = (req: Request, res: Response) => {
 //              implement simultaneous updating of dbt and crdt acc's
 export let update_acc = (req: Request, res: Response) => {
     //get old balance
-    let sql1 = "SELECT * FROM accounts WHERE account_id = " + req.body.account_id;
-    connection.query(sql1, (err, result) => {
+    //let sql1 = `SELECT * FROM accounts WHERE account_id = '${req.body.account_id}'`;
+    dbFunctions.query(`SELECT * FROM accounts WHERE account_id = '${req.body.account_id}'`, (err, result) => {
         if (err) 
             res.status(500).json(err)
         else {
             //calc new balance
             var new_balance = result[0].balance + req.body.amount;
             //update with new balance
-            let sql = "UPDATE accounts SET balance = " + new_balance + ", last_updated = DEFAULT WHERE account_id = " + req.body.account_id;
-            connection.query(sql, (err, result) => {
+            //let sql = `UPDATE accounts SET balance = '${new_balance}', last_updated = DEFAULT WHERE account_id = '${req.body.account_id}'`;
+            dbFunctions.query(`UPDATE accounts SET balance = '${new_balance}', last_updated = DEFAULT WHERE account_id = '${req.body.account_id}'`, (err, result) => {
                 if (err) 
                     res.status(500).json(err)
                 else
@@ -98,7 +97,7 @@ export let create_new_trans = (req: Request, res: Response) => {
 
 export let get_user_by_user_id = (req: Request, res: Response) => {
     let sql = "SELECT * FROM users WHERE user_id = " + req.params.user_id;
-    connection.query(sql, (err, result) => {
+    dbFunctions.query(sql, (err, result) => {
         if (err) 
             res.status(500).json(err)
         else
@@ -108,7 +107,7 @@ export let get_user_by_user_id = (req: Request, res: Response) => {
 
 export let get_user_by_user_name = (req: Request, res: Response) => {
     let sql = "SELECT * FROM users WHERE user_name = '" + req.params.user_name + "'";
-    connection.query(sql, (err, result) => {
+    dbFunctions.query(sql, (err, result) => {
         if (err) 
             res.status(500).json(err)
         else
@@ -118,7 +117,7 @@ export let get_user_by_user_name = (req: Request, res: Response) => {
 
 export let create_new_user = (req: Request, res: Response) => {
     let sql = "INSERT INTO users (user_name, date_created, active) VALUES ('" + req.params.user_name + "', DEFAULT, " + 1 + ")";
-    connection.query(sql, (err, result) => {
+    dbFunctions.query(sql, (err, result) => {
         if (err) 
             res.status(500).json(err)
         else
@@ -128,7 +127,7 @@ export let create_new_user = (req: Request, res: Response) => {
 
 export let deactivate_status_of_user_id = (req: Request, res: Response) => {
     let sql = "UPDATE users SET active = 0 WHERE user_id = " + req.params.user_id;
-    connection.query(sql, (err, result) => {
+    dbFunctions.query(sql, (err, result) => {
         if (err) 
             res.status(500).json(err)
         else
