f284123efe1bad5b4e4887a27904cb54d6d145b5
revan-wy <revan-wy@student.wethinkcode.co.za>
2019-02-14 21:11:39 +0200

    endpoint routing was updated

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import * as express         from "express";
 import * as exchange        from "./controllers/exchangeController";
 import * as jwtController   from "./controllers/jwtcontroller";
 import * as status          from "./controllers/status";
-import * as transaction     from "./controllers/transaction"; 
+import * as transactions     from "./controllers/transactions"; 
 import * as wallet          from "./controllers/wallet";
 import * as dbFunctions     from "./db";
 import * as middleware      from "./middleware";
@@ -19,28 +19,20 @@ app.get("/getToken", jwtController.genToken);
 
 //users table description: user_id, user_name, date_created, active
 //USERS ENDPOINTS ARE UNTESTED
-app.get ('/user/:user_id', users.get_user_by_user_id);
-app.get ('/user/:user_name', users.get_user_by_user_name);
-app.post('/user/:user_name', users.create_new_user);
-app.put ('/user/:user_id/status', users.deactivate_status_of_user_id);
+app.get ('/users/:user_id', users.get_user_by_user_id);
+app.get ('/users/:user_name', users.get_user_by_user_name);
+app.post('/users/:user_name', users.create_new_user);
+app.put ('/users/:user_id/status', users.deactivate_status_of_user_id); //is this route name ok? refer to staff
 
-app.get ('/accounts/owner_user_id/:owner_user_id', status.get_acc_by_owner_user_id); 
-app.get ('/accounts/account_id/:account_id', status.get_acc_by_account_id);
+app.get ('/accounts/:owner_user_id', status.get_accs_by_owner_user_id); 
+app.get ('/accounts/:account_id', status.get_acc_by_account_id);
 app.post('/accounts', status.create_new_acc);
 app.put ('/accounts', status.update_acc); //json fields: account_id, amount
 
-app.get("/transactions", transaction.transactions);
-app.get("/transaction/:id/", transaction.getTransaction);
-app.post("/transaction", middleware.verifyToken, transaction.addTransaction);
-app.delete("/transaction/:id", middleware.verifyToken, transaction.delTransaction); //consider for deprecation
-app.put("/transaction/:id/:execute*?", middleware.verifyToken, transaction.updateTransaction);
-
-//cosider block for deprecation
-app.get("/wallets", wallet.wallets);
-app.get("/wallet/:id/:transactions*?", wallet.getWallet);
-app.post("/wallet", middleware.verifyToken, wallet.addWallet);
-app.delete("/wallet/:id", middleware.verifyToken, wallet.delWallet); //consider for deprecation
-app.put("/wallet/:id", middleware.verifyToken, wallet.updateWallet);
+app.get("/transactions", transactions.transactions);
+app.get("/transaction/:id/", transactions.getTransaction);
+app.post("/transactions", middleware.verifyToken, transactions.addTransaction);
+app.put("/transactions/:id/:execute*?", middleware.verifyToken, transactions.updateTransaction); //this will be refined at a later stage
 
 app.get("/exchange", exchange.getRates);
 
