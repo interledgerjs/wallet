997f4bfaaa154370e35f58c7727c85b4ce926286
TalPat <38129947+TalPat@users.noreply.github.com>
2019-02-15 13:08:26 +0200

    New database schema incorporated (#2)
    * cleanup
    
    * minor changes
    
    * sql updated
    
    * database retrofit
    
    * updated sql
    
    * exchangeable databases
    
    * double database integration

--- src/controllers/wallet.ts
+++ src/controllers/wallet.ts
@@ -5,27 +5,31 @@ import * as jwt from "jsonwebtoken";
 
 //get /wallet #returns all wallets
 export let wallets = (req: Request, res: Response) => {
-    dbFunctions.queryRet("SELECT * FROM wallets", (result) => {
-        if (result.length == 0) {
-            res.sendStatus(404);
+    dbFunctions.query("SELECT * FROM accounts", (err, result) => {
+        if (err)
+            res.status(500).send(err);
+        else {
+            if (result.length == 0)
+                res.sendStatus(404);
+            else
+                res.json(result);
         }
-        else
-            res.json(result);
     });
 }
 
 //get /wallet/{1} #returns wallet with id 1
 export let getWallet = (req: Request, res: Response) => {
+    //routing needs to be redone for /accounts/{id}/transactions
     if (req.params.transactions == 'transactions') {
-        dbFunctions.queryRet(`SELECT *
-                            FROM transactions
-                            WHERE destid = '${req.params.id}' OR sourceid = '${req.params.id}'`
-                            , (result) => {
-            if (result.length == 0) {
-                res.sendStatus(404);
+        dbFunctions.query(`SELECT * FROM transactions WHERE destid = '${req.params.id}' OR sourceid = '${req.params.id}'`, (err, result) => {
+            if (err)
+                res.status(500).send(err);
+            else {
+                if (result.length == 0)
+                    res.sendStatus(404);
+                else
+                    res.json(result);
             }
-            else
-                res.json(result);
         });
     }
     else if (req.params.transactions !== undefined) {
@@ -33,12 +37,15 @@ export let getWallet = (req: Request, res: Response) => {
         res.sendStatus(400);
     }
     else {
-        dbFunctions.queryRet(`SELECT * FROM wallets WHERE walletid = '${req.params.id}'`, (result) => {
-            if (result.length == 0) {
-                res.sendStatus(404);
+        dbFunctions.query(`SELECT * FROM accounts WHERE account_id = '${req.params.id}'`, (err, result) => {
+            if (err)
+                res.status(500).send(err);
+            else {
+                if (result.length == 0)
+                    res.sendStatus(404);
+                else
+                    res.json(result);
             }
-            else
-                res.json(result);
         });
     }
 }
@@ -51,8 +58,9 @@ export let addWallet = (req : Request, res: Response) => {
         }
         else {
             const schema = Joi.object().keys({
-                balance:Joi.number().required(),
-                holder: Joi.string().required()
+                account_name:Joi.string().required(),
+                owner_user_id: Joi.number().required(),
+                balance: Joi.number().required()
             });
             const result = Joi.validate(req.body, schema);
         //    console.log(result);
@@ -68,8 +76,12 @@ export let addWallet = (req : Request, res: Response) => {
                 }
                 if (keys.length > 0) keys = keys.slice(0, -1);
                 if (vals.length > 0) vals = vals.slice(0, -1);
-                dbFunctions.queryNoRet(`INSERT INTO wallets (${keys}) VALUES (${vals})`);
-                res.send(JSON.stringify(req.body));
+                dbFunctions.query(`INSERT INTO accounts (${keys}) VALUES (${vals})`, (err) => {
+                    if (err)
+                        res.status(500).send(err);
+                    else 
+                        res.send(JSON.stringify(req.body));
+                });
             }
         }
     });
@@ -82,13 +94,21 @@ export let delWallet = (req : Request, res: Response) => {
             res.status(403).send(err.message);
         }
         else {
-            dbFunctions.queryRet(`SELECT * FROM wallets WHERE walletid = '${req.params.id}'`, (result) => {
-                if (result.length == 0) {
-                    res.sendStatus(404);
-                }
+            dbFunctions.query(`SELECT * FROM accounts WHERE account_id = '${req.params.id}'`, (err, result) => {
+                if (err)
+                    res.status(500).send(err);
                 else {
-                    dbFunctions.queryNoRet(`DELETE FROM wallets where walletid = '${req.params.id}'`);
-                    res.send(`Wallet id: ${req.params.id} deleted`);
+                    if (result.length == 0) {
+                        res.sendStatus(404);
+                    }
+                    else {
+                        dbFunctions.query(`DELETE FROM accounts where account_id = '${req.params.id}'`, (err) => {
+                            if (err)
+                                res.status(500).send(err);
+                            else
+                                res.send(`Account id: ${req.params.id} deleted`);
+                        });
+                    }
                 }
             });
         }
@@ -103,9 +123,10 @@ export let updateWallet = (req : Request, res: Response) => {
         }
         else {
             const schema = Joi.object().keys({
-                balance:Joi.number(),
-                holder: Joi.string()
-            }).or('balance', 'holder');
+                account_name:Joi.string(),
+                owner_user_id: Joi.number(),
+                balance: Joi.number()
+            }).or('account_name', 'owner_user_id', 'balance');
             const result = Joi.validate(req.body, schema);
             if (result.error) {
                 res.sendStatus(400)
@@ -116,16 +137,26 @@ export let updateWallet = (req : Request, res: Response) => {
                     str += `${k}='${req.body[k]}',`;
                 }
                 if (str.length > 0) str = str.slice(0, -1);
-                dbFunctions.queryNoRet(`UPDATE wallets SET ${str} WHERE walletid = '${req.params.id}'`);
-                dbFunctions.queryRet(`SELECT * FROM wallets WHERE walletid = '${req.params.id}'`, (result) => {
-                    if (result.length == 0) {
-                    //    console.log(404);
-                        res.status(404);
-                        res.send("invalid id requested");
+                dbFunctions.query(`UPDATE accounts SET ${str}, last_updated = default WHERE account_id = '${req.params.id}'`, (err) => {
+                    if (err)
+                        res.status(500).send(err);
+                    else {
+                        dbFunctions.query(`SELECT * FROM accounts WHERE account_id = '${req.params.id}'`, (err, result) => {
+                            if (err)
+                                res.status(500).send(err);
+                            else {
+                                if (result.length == 0) {
+                                //    console.log(404);
+                                    res.status(404);
+                                    res.send("invalid id requested");
+                                }
+                                else
+                                    res.json(result);
+                            }
+                        });
                     }
-                    else
-                        res.json(result);
                 });
+                
             }
         }
     });
