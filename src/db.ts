a400ab2e3526aa02ac8a2f56907076b080f3b839
talpat <talonpatterson94@gmail.com>
2019-02-15 10:27:53 +0200

    double database integration

--- src/db.ts
+++ src/db.ts
@@ -50,7 +50,7 @@ export let initialise = () => {
                     user_name VARCHAR(255),\
                     date_created datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,\
                     active INT,\
-                    password VARCHAR(2047));", (err) => {
+                    password VARCHAR(255));", (err) => {
                         if (err) throw err;
                     });
                 connectDb.query("CREATE TABLE IF NOT EXISTS accounts (\
@@ -76,24 +76,9 @@ export let initialise = () => {
         console.log('sqlite db building');
 
         db.serialize(function() {
-            db.run("CREATE TABLE IF NOT EXISTS users (\
-                user_id INTEGER PRIMARY KEY AUTOINCREMENT,\
-                user_name VARCHAR(255),\
-                date_created datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,\
-                active INT,\
-                password VARCHAR(2047));");
-            db.run("CREATE TABLE IF NOT EXISTS accounts (\
-                account_id INTEGER PRIMARY KEY AUTOINCREMENT,\
-                account_name VARCHAR(255),\
-                owner_user_id INT,\
-                balance INT,\
-                last_updated datetime NOT NULL DEFAULT CURRENT_TIMESTAMP);");
-            db.run("CREATE TABLE IF NOT EXISTS transactions (\
-                trans_id INTEGER PRIMARY KEY AUTOINCREMENT,\
-                dbt_acc_id INT,\
-                crdt_acc_id INT,\
-                amount INT,\
-                date datetime NOT NULL DEFAULT CURRENT_TIMESTAMP);");
+            db.run("CREATE TABLE IF NOT EXISTS users (user_id INT AUTO_INCREMENT PRIMARY KEY, user_name VARCHAR(255), date_created datetime NOT NULL DEFAULT CURRENT_TIMESTAMP, active INT, password VARCHAR(255))"); 
+            db.run("CREATE TABLE IF NOT EXISTS accounts (account_id INT AUTO_INCREMENT PRIMARY KEY, account_name VARCHAR(255), owner_user_id INT, balance INT, last_updated datetime NOT NULL DEFAULT CURRENT_TIMESTAMP)");
+            db.run("CREATE TABLE IF NOT EXISTS transactions (trans_id INT AUTO_INCREMENT PRIMARY KEY, dbt_acc_id INT, crdt_acc_id INT, amount INT, date datetime NOT NULL DEFAULT CURRENT_TIMESTAMP)");    
         });
     }   
 }
