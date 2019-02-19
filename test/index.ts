602aaf9caefcecba5263365520546645c5bed4ad
TheReasonWhy <40379554+TheReasonWhy@users.noreply.github.com>
2019-02-19 14:49:42 +0200

    testing integrated (#20)
    * try to fix gitignore
    
    * try to fix gitignore
    
    * updated password for mysql
    
    * cleanup
    
    * minor changes
    
    * add accounts endpoints WIP
    
    * updated gitignore
    
    * sql updated
    
    * laid foundation for importing modules
    
    * database retrofit
    
    * updated sql
    
    * imported users and accounts modules
    
    * added users and accounts endpoints
    
    * clean up of status.ts
    
    * completed untested users
    
    * exchangeable databases
    
    * endpoint routing was updated
    
    * accounts module created
    
    * double database integration
    
    * WIP
    
    * added package-lock
    
    * added package-lock
    
    * updated dev/dependencies
    
    * updated readme
    
    * added dev dependencies from ilp-logger
    
    * added scripts from ilp-logger
    
    * added test.ts
    
    * updated tsconfig
    
    * added tslint.json
    
    * updated route for app.js
    
    * added dev dependencies for API testing
    
    * normed app.ts and started index.ts
    
    * accounts.ts tslint compliant
    
    * exchangeController.ts tslint compliant
    
    * jwtController tslint compliant
    
    * most tslint compliant
    
    * transactions tslint compliant
    
    * app.ts methos names updated to camelCase
    
    * implemented first test
    
    * implemented first test
    
    implemented first test
    
    * implemented first test
    
    implemented first test
    
    implemented first test
    
    Merge remote-tracking branch 'origin/tempo' into tempo
    
    * updated gitignore
    
    * WIP tslint
    
    * WIP tslint
    
    * WIP tslint
    
    * all tslint compliant
    
    * Delete accounts.ts
    
    * Delete transactions.ts

---
+++ test/index.ts
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const expect = require('chai').expect
+const request = require('supertest');
+const app = require('../build/app')
+
+describe('Unit testing the /users route', function() {
+    it('should return OK status', function() {
+      return request(app)
+        .get('/users/id/1')
+        .then(function(response){
+            assert.equal(response.status, 200)
+        })
+    });
+});
