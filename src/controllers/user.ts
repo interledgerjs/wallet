2abc1bf25edfa7c0d96e28689d283ee65eaf05f2
jadinek <jkruger@student.wethinkcode.co.za>
2019-02-20 17:25:29 +0200

    built user login controllers using data-layer

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express'
 import * as jwt from 'jsonwebtoken'
 import * as dlInterface from '../datalayer/dlInterface'
+import * as bcrypt from 'bcrypt'
+const saltRounds = 3
 
 export let users = (req: Request, res: Response) => {
   let dataParams = {
@@ -42,98 +44,124 @@ export let getuser = (req: Request, res: Response) => {
 }
 
 export let adduser = (req: Request, res: Response) => {
-  jwt.verify(req.token, 'secret', (err, authData) => {
+  const pass = req.body.password
+  bcrypt.genSalt(saltRounds, function (err: any, salt: any) {
     if (err) {
-      res.status(403).send(err.message)
+      throw err
     } else {
-
-      let dataParams = {
-        action: 'post',
-        table: 'users',
-        parameters: req.body
-      }
-      dlInterface.handleOp(dataParams, (err, result) => {
+      bcrypt.hash(pass, salt, function (err: any, hash: any) {
         if (err) {
-          res.status(500).send(err)
+          throw err
         } else {
-          res.send('User added')
+          req.body.password = hash
+          let dataParams = {
+            action: 'post',
+            table: 'users',
+            parameters: req.body
+          }
+          dlInterface.handleOp(dataParams, (err, result) => {
+            if (err) {
+              res.status(500).send(err)
+            } else {
+              res.send('User added')
+            }
+          })
+
         }
       })
-
     }
   })
-
 }
 
 export let deluser = (req: Request, res: Response) => {
-  jwt.verify(req.token, 'secret', (err, authData) => {
+  let dataParams = {
+    action: 'get',
+    table: 'users',
+    selectAll: true,
+    filter: [{ field: 'userID', operator: '=', value: req.params.id }]
+  }
+  dlInterface.handleOp(dataParams, (err, result) => {
     if (err) {
-      res.status(403).send(err.message)
+      res.status(500).send(err)
     } else {
-      let dataParams = {
-        action: 'get',
-        table: 'users',
-        selectAll: true,
-        filter: [{ field: 'userID', operator: '=', value: req.params.id }]
-      }
-      dlInterface.handleOp(dataParams, (err, result) => {
-        if (err) {
-          res.status(500).send(err)
-        } else {
-          if (result.length === 0) {
-            res.sendStatus(404)
-          } else {
-            let delParams = {
-              action: 'delete',
-              table: 'users',
-              filter: [{ field: 'userID', operator: '=', value: req.params.id }]
-            }
-            dlInterface.handleOp(delParams, (err, result) => {
-              if (err) {
-                res.status(500).send(err)
-              } else {
-                res.send(`user id: ${req.params.id} deleted`)
-              }
-            })
-          }
+      if (result.length === 0) {
+        res.sendStatus(404)
+      } else {
+        let delParams = {
+          action: 'delete',
+          table: 'users',
+          filter: [{ field: 'userID', operator: '=', value: req.params.id }]
         }
-      })
+        dlInterface.handleOp(delParams, (err, result) => {
+          if (err) {
+            res.status(500).send(err)
+          } else {
+            res.send(`user id: ${req.params.id} deleted`)
+          }
+        })
+      }
     }
   })
 }
 
 export let updateuser = (req: Request, res: Response) => {
-  jwt.verify(req.token, 'secret', (err, authData) => {
+  let dataParams = {
+    action: 'put',
+    table: 'users',
+    filter: [{ field: 'userID', operator: '=', value: req.params.id }],
+    parameters: req.body
+  }
+  dlInterface.handleOp(dataParams, (err, result) => {
     if (err) {
-      res.status(403).send(err.message)
+      res.status(500).send(err)
     } else {
-      let dataParams = {
-        action: 'put',
+      let getParams = {
+        action: 'get',
         table: 'users',
         filter: [{ field: 'userID', operator: '=', value: req.params.id }],
-        parameters: req.body
+        selectAll: true
       }
-      dlInterface.handleOp(dataParams, (err, result) => {
+      dlInterface.handleOp(getParams, (err, result) => {
         if (err) {
           res.status(500).send(err)
         } else {
-          let getParams = {
-            action: 'get',
-            table: 'users',
-            filter: [{ field: 'userID', operator: '=', value: req.params.id }],
-            selectAll: true
+          if (result.length === 0) {
+            res.status(404).send('invalid id requested')
+          } else {
+            res.json(result)
           }
-          dlInterface.handleOp(getParams, (err, result) => {
-            if (err) {
-              res.status(500).send(err)
-            } else {
-              if (result.length === 0) {
-                res.status(404).send('invalid id requested')
-              } else {
-                res.json(result)
-              }
-            }
+        }
+      })
+    }
+  })
+}
+
+export let login = (req: Request, res: Response) => {
+  let password = req.params.password
+  let dataParams = {
+    action: 'get',
+    table: 'users',
+    selectAll: true,
+    filter: [{ field: 'userName', operator: '=', value: `'${req.params.userName}'` }]
+  }
+  dlInterface.handleOp(dataParams, (err, result) => {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      bcrypt.compare(password, result[0].password, function (err: any, answer: any) {
+        if (err) {
+          throw err
+        }
+        if (answer) {
+          const user = {
+            userID: result[0].userID,
+            userName: result[0].userName
+          }
+          jwt.sign({ user }, 'secret',{ expiresIn: '1d' }, (_err, token) => {
+            res.json({ token })
           })
+        } else {
+          res.json('fail')
         }
       })
     }
