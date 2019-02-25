d79f425e3517d672408eb53fb4fc135a0005c53f
revan-wy <revan-wy@student.wethinkcode.co.za>
2019-02-25 14:55:08 +0200

    fixed index.ts

--- test/index.ts
+++ test/index.ts
@@ -1,12 +1,25 @@
-import assert from 'assert';
-import request from 'supertest';
-import app from '../build/app';
+const assert = require('assert');
 const expect = require('chai').expect
+const request = require('supertest');
+const app = require('../build/app')
 
 // this test uses a preset ID's
 // change the test to use whichever ID is assigned by the database
 
 let token
+
+// .get('/getToken')
+describe('Test to get get a bearer token', function() {
+  it('should return OK status', function() {
+    return request(app)
+      .get('/getToken')
+      .then(function(response){
+          assert.equal(response.status, 200)
+          token = response.body.token
+          // console.log(token)
+      })
+  });
+});
 
 // .post('/transaction')
 describe('Test to create a new transaction', function() { 
@@ -202,19 +215,6 @@ describe('Test to get a user by id', function() {
   });
 });
 
-// .get('/login/:userName') 
-describe('Test to log a user in', function() {
-  it('should return OK status', function() {
-    return request(app)
-      .get('/login/testuser3')
-      .send({"password": "test_user"})
-      .then(function(response){
-          //console.log(response)
-          assert.equal(response.status, 200)
-      })
-  });
-});
-
 // .put('/user/:id')
 describe('Test to update a user', function() {
   let data = {
@@ -225,6 +225,19 @@ describe('Test to update a user', function() {
       .put('/user/605')
       .send(data)
       .set('Authorization', 'Bearer ' + token)
+      .then(function(response){
+          //console.log(response)
+          assert.equal(response.status, 200)
+      })
+  });
+});
+
+// .get('/login/:userName') 
+describe('Test to log a user in', function() {
+  it('should return OK status', function() {
+    return request(app)
+      .get('/login/testuser3')
+      .send({"password": "test_user"})
       .then(function(response){
           //console.log(response)
           assert.equal(response.status, 200)
@@ -243,19 +256,6 @@ describe('Test to delete a user', function() {
             assert.equal(response.status, 200)
         })
     });
-});
-
-// .get('/getToken')
-describe('Test to get get a bearer token', function() {
-  it('should return OK status', function() {
-    return request(app)
-      .get('/getToken')
-      .then(function(response){
-          assert.equal(response.status, 200)
-          token = response.body.token
-          // console.log(token)
-      })
-  });
 });
 
 // .get('/exchange')
