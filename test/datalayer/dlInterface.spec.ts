db901f8a5f6ba421cf08b53cc76d8bbcaeb87c06
revan-wy <revan-wy@student.wethinkcode.co.za>
2019-02-26 11:48:27 +0200

    updated test to scalable syntax

--- test/datalayer/dlInterface.spec.ts
+++ test/datalayer/dlInterface.spec.ts
@@ -1,13 +1,13 @@
 import {assert} from 'chai';
 import { expect } from 'chai'
 import * as request from 'supertest';
-import {ftHandleDelete} from '../../build/datalayer/dlInterface';
+import * as dlInterface from '../../build/datalayer/dlInterface';
 
 describe('dlInterface ftHandleDelete', function() {
   it('should return a string', function() {
     let options = {
       'table': 'Renzo'
     }
-    assert.equal(typeof ftHandleDelete(options), 'string')
+    assert.equal(typeof dlInterface.ftHandleDelete(options), 'string')
   })
 })
