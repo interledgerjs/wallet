f17d6d6f9d2fa71e21d5ddb380943abbbddbca31
revan-wy <revan-wy@student.wethinkcode.co.za>
2019-03-05 12:19:03 +0200

    added guiding comments for creating functions

--- src/models/accounts.model.ts
+++ src/models/accounts.model.ts
@@ -5,3 +5,21 @@ export interface Account {
   balance: number,
   last_updated: string
 }
+
+// app.post('/users/:id/accounts', middleware.verifyToken, account.createAccount)
+  // body.accountID?, body.accountName?, body.ownerUserID?
+
+// app.get('/accounts', account.readAllAccounts)
+  // no required input
+
+// app.get('/users/:id/accounts', account.readAllAccountsByUserId)
+  // placeholder comment
+
+// app.get('/users/:id/accounts/:id', middleware.validateData, account.readAccount)
+  // id as param
+
+// app.put('/users/:id/accounts/:id', middleware.verifyToken, account.updateAccount)
+  // id as param, body.accountName, body.ownerUserID?
+
+// app.delete('/users/:id/accounts/:id', middleware.verifyToken, account.deleteAccount)
+  // id as param
