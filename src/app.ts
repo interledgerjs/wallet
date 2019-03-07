b96e52f15444a77ca872fe67fd24460159ac1e01
jadinek <jkruger@student.wethinkcode.co.za>
2019-03-07 13:43:50 +0200

    feat: createUser added

--- src/app.ts
+++ src/app.ts
@@ -1,63 +1,60 @@
+import * as dotenv from 'dotenv'
 import * as bodyParser from 'body-parser'
 import * as express from 'express'
-import * as account from './controllers/account'
-import * as jwtController from './controllers/jwtcontroller'
+// import * as jwtController from './controllers/jwtcontroller'
 import * as transaction from './controllers/transaction'
 import * as user from './controllers/user'
-import * as middleware from './middleware'
-import * as dotenv from 'dotenv'
 
 dotenv.config()
 const app = express()
 module.exports = app
-app.set('port', 3000)
 app.use(bodyParser.json())
 
-app.post('/transaction', middleware.verifyToken, transaction.addTransaction)
-  // body.transID, body.dbtAccID, body.crdtAccID, body.amount
-app.get('/transactions', transaction.transactions)
-  // no required input
-app.get('/transaction/:id/', middleware.validateData, transaction.getTransaction)
-  // id as param
-app.put('/transaction/:id', middleware.verifyToken, transaction.updateTransaction)
-  // id as param, body.dbtAccID, body.crdtAccID, body.amount
-app.delete('/transaction/:id', middleware.verifyToken, transaction.delTransaction)
-  // id as param
+app.post('/transactions', transaction.createTransaction)
+  // body.transID?, body.dbtAccID, body.crdtAccID, body.amount
+app.get('/transactions/', transaction.readTransactions)
+// no required input
+app.get('/transactions/id/:id/', transaction.readTransactionByID)
+// id as param
+app.get('/transactions/account/:accountID/', transaction.readTransactionByAccount)
+// account id as param
 
-app.post('/account', middleware.verifyToken, account.addAccount)
-  // body.accountID?, body.accountName?, body.ownerUserID?
-app.get('/accounts', account.accounts)
-  // no required input
-app.get('/account/:id', middleware.validateData, account.getAccount)
-  // id as param
-app.put('/account/:id', middleware.verifyToken, account.updateAccount)
-  // id as param, body.accountName, body.ownerUserID?
-app.delete('/account/:id', middleware.verifyToken, account.delAccount)
-  // id as param
+// app.post('/account', middleware.verifyToken, account.addAccount)
+//   // body.accountID?, body.accountName?, body.ownerUserID?
+// app.get('/accounts', account.accounts)
+//   // no required input
+// app.get('/account/:id', middleware.validateData, account.getAccount)
+//   // id as param
+// app.put('/account/:id', middleware.verifyToken, account.updateAccount)
+//   // id as param, body.accountName, body.ownerUserID?
+// app.delete('/account/:id', middleware.verifyToken, account.delAccount)
+//   // id as param
 
-app.post('/user', middleware.verifyToken, user.addUser)
-  // body.userName?, body.password?, body.active?
-app.get('/users', user.users)
+// app.post('/user', user.createUser)
+
+app.get('/users', user.readUser)
   // no required input
-app.get('/user/:id', middleware.validateData, user.getuser)
+app.get('/users/id/:id', user.readUserByID)
   // id as param
-app.get('/login/:userName', user.login)
-  // userName as param, body.password
-app.put('/user/:id', middleware.verifyToken, user.updateuser)
-  // id as param, body.userName?, body.active?, body.password?
-app.delete('/user/:id', middleware.verifyToken, user.deluser)
-  // id as param
-app.get('/user/userName/:userName', middleware.validateData, user.getUserByUserName)
+app.get('/users/username/:username', user.readUserByUserName)
 // userName as param
+// app.get('/login/:userName', user.login)
+  // userName as param, body.password
+// app.put('/user/:id', middleware.verifyToken, user.updateuser)
+  // id as param, body.userName?, body.active?, body.password?
+// app.delete('/user/:id', middleware.verifyToken, user.deluser)
+  // id as param
+app.post('/users', user.createUser)
+  // body.userName?, body.password?
 
-app.get('/getToken', jwtController.genToken)
+// app.get('/getToken', jwtController.genToken)
 
 app.all('*', (req, res) => {
   res.sendStatus(404)
 })
 
 if (!module.parent) {
-  app.listen(app.get('port'), () => {
-    console.log('server running on port %d', app.get('port'))
+  app.listen(process.env.PORT, () => {
+    console.log('server running on port %d', process.env.PORT)
   })
 }
---
+++ src/models/user.ts
@@ -0,0 +1,83 @@
+import { Request, Response } from 'express'
+import * as dbFunctions from './db'
+
+export interface User {
+  userID: number,
+  userName: string,
+  dateCreated: string,
+  active: boolean,
+  password: string
+}
+function isUser (user: any): user is User {
+  return (
+    typeof user.userID === 'number' &&
+    typeof user.userName === 'string' &&
+    typeof user.dateCreated === 'string' &&
+    typeof user.active === 'boolean' &&
+    typeof user.password === 'string'
+  )
+}
+// functions
+
+// function to handle getting all users
+export function readUser (callback: (error: Boolean, result: User[] | null) => void) {
+  const sql = `SELECT * FROM users`
+  dbFunctions.query(sql, function (err: object, result: User[]) {
+    if (err) {
+      callback(true, null)
+      console.log(err)
+    } else {
+      callback(false, result)
+    }
+  })
+}
+
+// function to handle get user by id
+export function readUserByID (userID: number, callback: (error: Boolean, result: User | null) => void) {
+  const sql = `SELECT * FROM users where userID = '${userID}'`
+  dbFunctions.query(sql, function (err: object, result: User[]) {
+    if (err) {
+      callback(true, null)
+      console.log(err)
+    } else {
+      if (result.length > 0) {
+        callback(false, result[0])
+      } else {
+        callback(false, null)
+      }
+    }
+  })
+}
+
+// function to handle get user by userName
+export function readUserByUserName (userName: string, callback: (error: Boolean, result: User | null) => void) {
+  const sql = `SELECT * FROM users where userName = '${userName}'`
+  dbFunctions.query(sql, function (err: object, result: User[]) {
+    if (err) {
+      callback(true, null)
+      console.log(err)
+    } else {
+      if (result.length > 0) {
+        callback(false, result[0])
+      } else {
+        callback(false, null)
+      }
+    }
+  })
+}
+// function to handle adding users
+export function createUser (user: User, callback: (error: Boolean) => void) {
+  if (isUser(user)) {
+    const sql = `INSERT INTO users (userName, dateCreated, active, password) VALUES ('${user.userName}', '${user.dateCreated}', ${user.active}, '${user.password}')`
+    dbFunctions.query(sql, function (err: object) {
+      if (err) {
+        callback(true)
+        console.log(err)
+      } else {
+        callback(false)
+      }
+    })
+  } else {
+    callback(true)
+  }
+}
