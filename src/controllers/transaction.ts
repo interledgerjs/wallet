ebc0bf2e0de6f74ef5a7384f8ec6ec87b27ddf3c
Talon Patterson <38129947+TalPat@users.noreply.github.com>
2019-04-01 15:33:41 +0200

    Changed callbacks to async-await calls (#69)
    * transactions functioning
    
    * user changes
    
    * user changes
    
    * users functional
    
    * accounts functioning
    
    * typo fix
    
    * updateaccount fix
    
    * commented out console logs
    
    * commented out console logs

--- src/controllers/transaction.ts
+++ src/controllers/transaction.ts
@@ -1,14 +1,14 @@
 import { Request, Response } from 'express'
-import * as transaction from '../models/transaction'
+import { Transaction, addTransaction, retrieveTransactions, retrieveTransactionByID, retrieveTransactionsByAccID } from '../models/transaction'
 
 // post /transactions #adds new transaction to table
-export function createTransaction (req: Request, res: Response) {
+export async function createTransaction (req: Request, res: Response) {
   if (
     req.body.dbtAccID && typeof req.body.dbtAccID === 'number' &&
     req.body.crdtAccID && typeof req.body.crdtAccID === 'number' &&
     req.body.amount && typeof req.body.amount === 'number'
   ) {
-    const transObject: transaction.Transaction = {
+    const transObject: Transaction = {
       transID: -1,
       dbtAccID: req.body.dbtAccID,
       crdtAccID: req.body.crdtAccID,
@@ -18,75 +18,80 @@ export function createTransaction (req: Request, res: Response) {
     if (req.body.transID && typeof req.body.dbtAccID === 'number') {
       transObject.transID = req.body.transID
     }
-    transaction.createTransaction(transObject, function (error) {
-      if (error) {
-        res.status(500).send('Unable to add transaction')
-      } else {
+
+    try {
+      const failure = await addTransaction(transObject)
+      if (!failure) {
         res.send('Transaction added')
       }
-    })
+    } catch (error) {
+      // console.log(error)
+      res.status(500).send('unable to add transaction')
+    }
   } else {
     res.status(400).send('Bad request')
   }
 }
 
 // get /transactions #returns all transactions
-export function readTransactions (req: Request, res: Response) {
-  transaction.readTransactions(function (error, result) {
-    if (error) {
-      res.status(500).send('Unable to retrieve transactions')
+export async function readTransactions (req: Request, res: Response) {
+
+  try {
+    const result = await retrieveTransactions()
+    if (result.length === 0) {
+      res.sendStatus(404)
     } else {
-      if (result.length === 0) {
-        res.sendStatus(404)
-      } else {
-        res.send(result)
-      }
+      res.send(result)
     }
-  })
+  } catch (error) {
+    res.status(500).send('Unable to retrieve transactions')
+  }
 }
 
 // get /transactions/id/:id #returns single transaction by id
-export function readTransactionByID (req: Request, res: Response) {
+export async function readTransactionByID (req: Request, res: Response) {
   if (
     req.params.id &&
     !isNaN(parseInt(req.params.id, 10))
   ) {
     const transID: number = req.params.id
-    transaction.readTransactionByID(transID, function (error, result) {
-      if (error) {
-        res.status(500).send('Unable to retrieve transaction')
+
+    try {
+      const result = await retrieveTransactionByID(transID)
+      if (!result) {
+        res.sendStatus(404)
       } else {
-        if (!result) {
-          res.sendStatus(404)
-        } else {
-          res.send(result)
-        }
+        res.send(result)
       }
-    })
+    } catch (error) {
+      // console.log(error)
+      res.status(500).send('Unable to retrieve transaction')
+    }
   } else {
     res.status(400).send('Bad request')
   }
 }
 
 // get /transactions/account/:accountID #returns transaction array by account ids
-export function readTransactionByAccount (req: Request, res: Response) {
+export async function readTransactionByAccount (req: Request, res: Response) {
   if (
     req.params.accountID &&
     !isNaN(parseInt(req.params.accountID, 10))
   ) {
-    console.log(`value is '${req.params.accountID}'`)
+    // console.log(`value is '${req.params.accountID}'`)
     const accountID: number = req.params.accountID
-    transaction.readTransactionsByAccID(accountID, function (error, result) {
-      if (error) {
-        res.status(500).send('Unable to retrieve transactions')
+
+    try {
+      const result = await retrieveTransactionsByAccID(accountID)
+      if (result.length === 0) {
+        res.sendStatus(404)
       } else {
-        if (!result) {
-          res.sendStatus(404)
-        } else {
-          res.send(result)
-        }
+        res.send(result)
       }
-    })
+    } catch (error) {
+      // console.log(error)
+      res.status(500).send('Unable to retrieve transactions')
+    }
   } else {
     res.status(400).send('Bad request')
   }
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,107 +1,140 @@
-import { Request, Response } from 'express'
-import * as dbFunctions from './db'
+import { query } from './db'
 
 export interface User {
   userID: number,
   userName: string,
   dateCreated: string,
-  active: boolean,
+  active: number,
   pssword: string
 }
+
 function isUser (user: any): user is User {
   return (
     typeof user.userID === 'number' &&
     typeof user.userName === 'string' &&
     typeof user.dateCreated === 'string' &&
-    typeof user.active === 'boolean' &&
-    typeof user.password === 'string'
+    typeof user.active === 'number' &&
+    typeof user.pssword === 'string'
+  )
+}
+
+function isUserArray (result: any): result is User[] {
+  let isUserArray: boolean = true
+  if (result.length) {
+    result.forEach(function (element) {
+      if (!isUser(element)) {
+        isUserArray = false
+      }
+    })
+  }
+  return (
+    isUserArray || result === null
   )
 }
 // functions
 
 // function to handle getting all users
-export function readUser (callback: (error: Boolean, result: User[] | null) => void) {
-  const sql = `SELECT * FROM users`
-  dbFunctions.query(sql, function (err: object, result: User[]) {
-    if (err) {
-      callback(true, null)
-      console.log(err)
-    } else {
-      callback(false, result)
+export function retrieveUser (): Promise<User[]> {
+  return new Promise(async function (resolve, reject) {
+    const sql: string = `SELECT * FROM users`
+    try {
+      const result = await query(sql)
+      if (isUserArray(result)) {
+        resolve(result)
+      } else {
+        reject(true)
+      }
+    } catch (error) {
+      reject(error)
     }
   })
 }
 
 // function to handle get user by id
-export function readUserByID (userID: number, callback: (error: Boolean, result: User | null) => void) {
-  const sql = `SELECT * FROM users where userID = '${userID}'`
-  dbFunctions.query(sql, function (err: object, result: User[]) {
-    if (err) {
-      callback(true, null)
-      console.log(err)
-    } else {
-      if (result.length > 0) {
-        callback(false, result[0])
+export function retrieveUserByID (userID: number): Promise<User> {
+  return new Promise(async function (resolve, reject) {
+    const sql: string = `SELECT * FROM users where userID = '${userID}'`
+    try {
+      const result = await query(sql)
+      if (isUserArray(result)) {
+        if (result.length > 0) {
+          resolve(result[0])
+        } else {
+          resolve(null)
+        }
       } else {
-        callback(false, null)
+        reject(true)
       }
+    } catch (error) {
+      reject(error)
     }
   })
 }
 
 // function to handle get user by userName
-export function readUserByUserName (userName: string, callback: (error: Boolean, result: User | null) => void) {
-  const sql = `SELECT * FROM users where userName = '${userName}'`
-  dbFunctions.query(sql, function (err: object, result: User[]) {
-    if (err) {
-      callback(true, null)
-      console.log(err)
-    } else {
-      if (result.length > 0) {
-        callback(false, result[0])
+export function retrieveUserByUserName (userName: string): Promise<User> {
+  return new Promise(async function (resolve, reject) {
+    const sql: string = `SELECT * FROM users WHERE username = '${userName}'`
+    try {
+      const result = await query(sql)
+      // console.log(result)
+      if (isUserArray(result)) {
+        if (result.length > 0) {
+          resolve(result[0])
+        } else {
+          resolve(null)
+        }
       } else {
-        callback(false, null)
+        // console.log(isUserArray(result))
+        reject(true)
       }
+    } catch (error) {
+      reject(error)
     }
   })
 }
+
 // function to handle adding users
-export function createUser (user: User, callback: (error: Boolean) => void) {
-  if (isUser(user)) {
-    const sql = `INSERT INTO users (userName, dateCreated, active, password) VALUES ('${user.userName}', '${user.dateCreated}', ${user.active}, '${user.pssword}')`
-    dbFunctions.query(sql, function (err: object) {
-      if (err) {
-        callback(true)
-        console.log(err)
-      } else {
-        callback(false)
+export function addUser (user: User): Promise<boolean> {
+  return new Promise(async function (resolve, reject) {
+    if (isUser(user)) {
+      const sql: string = `INSERT INTO users (userName, dateCreated, active, pssword) VALUES ('${user.userName}', '${user.dateCreated}', ${user.active}, '${user.pssword}')`
+      try {
+        const result = query(sql)
+        if (isUserArray(result)) {
+          resolve(false)
+        } else {
+          resolve(true)
+        }
+      } catch (error) {
+        reject(error)
       }
-    })
-  } else {
-    callback(true)
-  }
-}
-
-export function updateUser (user: User, callback: (error: Boolean) => void) {
-  const sql = `UPDATE users SET userName = '${user.userName}', active = ${user.active}, pssword = '${user.pssword}' WHERE userID = '${user.userID}'`
-  dbFunctions.query(sql, function (err: object) {
-    if (err) {
-      callback(true)
-      console.log(err)
     } else {
-      callback(false)
+      resolve(true)
     }
   })
 }
 
-export function deleteUser (userID: number, callback: (error: Boolean) => void) {
-  const sql = `DELETE FROM users WHERE userID = '${userID}'`
-  dbFunctions.query(sql, function (err: object) {
-    if (err) {
-      callback(true)
-      console.log(err)
-    } else {
-      callback(false)
+export function alterUser (user: User): Promise<boolean> {
+  return new Promise(async function (resolve, reject) {
+    const sql: string = `UPDATE users SET userName = '${user.userName}', active = ${user.active}, pssword = '${user.pssword}' WHERE userID = '${user.userID}'`
+    try {
+      const result = await query(sql)
+      resolve(false)
+    } catch (error) {
+      reject(error)
+    }
+  })
+}
+
+export function removeUser (userID: number): Promise<boolean> {
+  return new Promise(async function (resolve, reject) {
+    const sql: string = `DELETE FROM users WHERE userID = '${userID}'`
+    try {
+      const result = await query(sql)
+      resolve(false)
+    } catch (error) {
+      reject(error)
     }
   })
 }
