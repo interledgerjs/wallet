8c6bd025eb14e9164f0786633086a6164182b2c9
jadinek <36467054+jadinek@users.noreply.github.com>
2019-04-04 17:29:41 +0200

    refined userController; added token endpoint (#87)
    refined userController; added token endpoint

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,8 +1,5 @@
 import { Request, Response } from 'express'
-// import * as jwt from 'jsonwebtoken'
-import { retrieveUser, retrieveUserByID, retrieveUserByUserName, addUser, modifyUser, removeUser, User } from '../models/userModel'
-import * as bcrypt from 'bcrypt'
-const saltRounds = 3
+import { retrieveUser, retrieveUserByID, retrieveUserByUserName, addUser, addAdmin, modifyUser, removeUser, User, hashing } from '../models/userModel'
 
 // get /user #returns all users
 export async function readUser (req: Request, res: Response) {
@@ -53,25 +50,35 @@ export async function readUserByUserName (req: Request, res: Response) {
 export async function createUser (req: Request, res: Response) {
   // check if userName already exists
   const userName: string = req.body.userName
-
+  const pssword: string = req.body.pssword
   try {
     const userExists = await retrieveUserByUserName(userName)
     if (!userExists) {
-      const salt = await bcrypt.genSalt(saltRounds)
-      const hash = await bcrypt.hash(req.body.pssword, salt)
-      const userObject: User = {
-        userID: -1,
-        userName: req.body.userName,
-        dateCreated: new Date().toISOString(),
-        deletedAt: '',
-        userRole: req.body.userRole,
-        pssword: hash
+      const userObject = await hashing(pssword, userName)
+      if (userObject) {
+        const result = await addUser(userObject)
+        res.sendStatus(200)
       }
-      const result = await addUser(userObject)
-      if (!result) {
-        res.send('User created')
-      } else {
-        res.sendStatus(400)
+    } else {
+      res.sendStatus(400)
+    }
+  } catch (error) {
+    res.sendStatus(500)
+  }
+}
+
+// post /users #adds new admin to table
+export async function createAdmin (req: Request, res: Response) {
+  // check if userName already exists
+  const userName: string = req.body.userName
+  const pssword: string = req.body.pssword
+  try {
+    const userExists = await retrieveUserByUserName(userName)
+    if (!userExists) {
+      const userObject = await hashing(pssword, userName)
+      if (userObject) {
+        const result = await addAdmin(userObject)
+        res.sendStatus(200)
       }
     } else {
       res.sendStatus(400)
@@ -83,79 +90,36 @@ export async function createUser (req: Request, res: Response) {
 
 // put /user/:id
 export async function updateUser (req: Request, res: Response) {
-  if (
-    req.params.id &&
-    !isNaN(parseInt(req.params.id, 10)) &&
-    (req.body.userName === undefined || typeof req.body.userName === 'string') &&
-    (req.body.dateCreated === undefined || typeof req.body.dateCreated === 'string') &&
-    (req.body.deletedAt === undefined || typeof req.body.deletedAt === 'boolean') &&
-    (req.body.userRole === undefined || typeof req.body.userRole === 'string') &&
-    (req.body.pssword === undefined || typeof req.body.pssword === 'string')
-
-  ) {
-    try {
-      const userExists = await retrieveUserByID(req.params.id)
-      if (userExists) {
-        const userObject: User = {
-          userID: userExists.userID,
-          userName: userExists.userName,
-          dateCreated: userExists.dateCreated,
-          deletedAt: userExists.deletedAt,
-          userRole: userExists.userRole,
-          pssword: userExists.pssword
-        }
-        if (req.body.userName !== undefined) {
-          userObject.userName = req.body.userName
-        }
-        if (req.body.dateCreated !== undefined) {
-          userObject.dateCreated = req.body.dateCreated
-        }
-        if (req.body.deletedAt !== undefined) {
-          userObject.deletedAt = req.body.deletedAt
-        }
-        if (req.body.userRole !== undefined) {
-          userObject.userRole = req.body.userRole
-        }
-        if (req.body.pssword !== undefined) {
-          const salt = await bcrypt.genSalt(saltRounds)
-          const hash = await bcrypt.hash(req.body.pssword, salt)
-          userObject.pssword = hash
-        }
-        const result = await modifyUser(userObject)
-        if (!result) {
-          res.send('Successfully updated user')
-        }
+  try {
+    const userExists = await retrieveUserByID(req.params.id)
+    if (userExists) {
+      const result = await modifyUser(userExists, req.body)
+      if (!result) {
+        res.sendStatus(200)
       } else {
         res.sendStatus(404)
       }
-    } catch (error) {
-      res.sendStatus(500)
+    } else {
+      res.sendStatus(404)
     }
-  } else {
-    res.status(400).send('Bad request')
+  } catch (error) {
+    res.sendStatus(500)
   }
 }
 
 // delete /user/:id
 export async function deleteUser (req: Request, res: Response) {
-  if (
-    req.params.id &&
-    !isNaN(parseInt(req.params.id, 10))
-  ) {
-    try {
-      const userExists = await retrieveUserByID(req.params.id)
-      if (userExists) {
-        const result = await removeUser(req.params.id)
-        if (!result) {
-          res.send('Successfully deleted user')
-        }
-      } else {
-        res.sendStatus(404)
+  try {
+    const userExists = await retrieveUserByID(req.params.id)
+    if (userExists) {
+      const result = await removeUser(req.params.id)
+      if (!result) {
+        res.sendStatus(200)
       }
-    } catch (error) {
-      res.sendStatus(500)
+    } else {
+      res.sendStatus(404)
     }
-  } else {
-    res.status(400).send('Bad request')
+  } catch (error) {
+    res.sendStatus(500)
   }
 }
---
+++ src/models/tokenModel.ts
@@ -0,0 +1,18 @@
+import { User, retrieveUserByUserName } from '../models/userModel'
+import * as bcrypt from 'bcrypt'
+import { token } from '../controllers/tokenController'
+
+export async function compareHash (userExists: User, pssword: string) {
+  return new Promise(async function (resolve, reject) {
+    try {
+      const compare = bcrypt.compareSync(pssword, userExists.pssword)
+      if (!compare) {
+        resolve(false)
+      } else {
+        resolve(true)
+      }
+    } catch (error) {
+      reject(error)
+    }
+  })
+}
