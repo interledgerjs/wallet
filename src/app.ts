3dbe0b2280b198d42e46ef4ea9ff6964587f5503
jadinek <36467054+jadinek@users.noreply.github.com>
2019-04-09 13:00:33 +0200

    updated database column names (#120)

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv'
 import * as express from 'express'
 import * as account from './controllers/accountController'
 import * as transaction from './controllers/transactionController'
-import { readUser, readUserByID, readUserByUserName, createUser, createAdmin, updateUser, deleteUser } from './controllers/userController'
+import { readUser, readUserById, readUserByUserName, createUser, createAdmin, updateUser, deleteUser } from './controllers/userController'
 import { token } from './controllers/tokenController'
 
 dotenv.config()
@@ -11,23 +11,23 @@ const app = express()
 module.exports = app
 app.use(bodyParser.json())
 
-app.post('/transactions', transaction.createTransaction) // body.transID?, body.dbtAccID, body.crdtAccID, body.amount
+app.post('/transactions', transaction.createTransaction) // body.id?, body.debitAccount, body.creditAccount, body.amount
 app.get('/transactions/', transaction.readTransactions) // no required input
-app.get('/transactions/id/:id/', transaction.readTransactionByID) // id as param
-app.get('/transactions/account/:accountID/', transaction.readTransactionByAccount) // account id as param
+app.get('/transactions/id/:id/', transaction.readTransactionById) // id as param
+app.get('/transactions/account/:id/', transaction.readTransactionByAccount) // account id as param
 
-app.post('/accounts', account.createAccount) // body.balance , body.accountName, body.ownerUserID
-app.get('/accounts/id/:accountid', account.readAccountByID) // id's as param
-app.get('/accounts/userid/:userid/', account.readAllAccountsByUserID)
+app.post('/accounts', account.createAccount) // body.balance , body.name, body.owner
+app.get('/accounts/id/:id', account.readAccountById) // id's as param
+app.get('/accounts/id/:id/', account.readAllAccountsById)
 app.get('/accounts', account.readAllAccounts) // no required input
-app.put('/accounts/:id', account.updateAccount) // id as param, body.accountName, body.ownerUserID, body.balance
+app.put('/accounts/:id', account.updateAccount) // id as param, body.name, body.owner, body.balance
 app.delete('/accounts/:id', account.deleteAccount) // id's as params
 
 app.post('/users', createUser) // body.userName, body.password
 app.get('/users', readUser)
-app.get('/users/id/:id', readUserByID) // id as param
+app.get('/users/id/:id', readUserById) // id as param
 app.get('/users/username/:username', readUserByUserName) // userName as param, body.pssword
-app.put('/users/:id', updateUser) // id as param, body.userName?, body.deletedAt?, body.pssword?, body.userRole?
+app.put('/users/:id', updateUser) // id as param, body.userName?, body.deletedAt?, body.pssword?, body.role?
 app.delete('/users/:id', deleteUser) // id as param
 
 app.post('/admin', createAdmin) // body.userName, body.password
