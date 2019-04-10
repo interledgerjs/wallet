8aeba1b33bd5b4e5576e324eec0fe959135b64c8
Talon Patterson <38129947+TalPat@users.noreply.github.com>
2019-04-10 12:53:27 +0200

    Revert "user authorisation added (#121)" (#128)
    This reverts commit a46734fb70508ec06e4b7e3764ab777d4870b6ed.

--- src/services/jwtService.ts
+++ src/services/jwtService.ts
@@ -1,7 +1,5 @@
 import { Request, Response } from 'express'
 import * as jwt from 'jsonwebtoken'
-import { User } from '../models/userModel'
-import { token } from '../controllers/tokenController';
 
 // verifyToken
 export function verifyToken (req: Request, res: Response, next: any) {
@@ -12,40 +10,16 @@ export function verifyToken (req: Request, res: Response, next: any) {
     const bearerToken: string = bearer[1]
     req['token'] = bearerToken
 
-        // verifies token and returns tokenData
-    jwt.verify(req.token, process.env.SECRETKEY, { algorithms: ['HS256'] }, (err: Error, tokenData: any) => {
+        // verifies token and returns authData
+    jwt.verify(req.token, process.env.SECRETKEY, { algorithms: ['HS256'] }, (err: Error, authData) => {
       if (err) {
         res.sendStatus(403)
       } else {
-        req.authData = tokenData
+        req.authData = authData
         next()
       }
     })
   } else {
     res.sendStatus(403)
-  }
-}
-
-export function verifyRoleToken (roles: string) {
-  return function (req: Request, res: Response, next: any) {
-    const bearerHeader: string = req.headers['authorization']
-    if (bearerHeader) {
-            // pulls token out of header
-      const bearer: string[] = bearerHeader.split(' ')
-      const bearerToken: string = bearer[1]
-      req['token'] = bearerToken
-
-            // verifies token and returns authData
-      jwt.verify(req.token, process.env.SECRETKEY, { algorithms: ['HS256'] }, (err: Error, tokenData: any) => {
-        if (err || tokenData.authData.userRole !== roles) {
-          res.sendStatus(403)
-        } else {
-          req.authData = tokenData
-          next()
-        }
-      })
-    } else {
-      res.sendStatus(403)
-    }
   }
 }
