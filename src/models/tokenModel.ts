9e7305c4a8bfbc2c05dac9168513a631dd3f966f
Talon Patterson <38129947+TalPat@users.noreply.github.com>
2019-04-10 13:00:43 +0200

    removed unnescessary async (#129)

--- src/models/tokenModel.ts
+++ src/models/tokenModel.ts
@@ -2,8 +2,8 @@ import { User, retrieveUserByUserName } from '../models/userModel'
 import * as bcrypt from 'bcrypt'
 import { token } from '../controllers/tokenController'
 
-export async function compareHash (userExists: User, pssword: string) {
-  return new Promise(async function (resolve, reject) {
+export function compareHash (userExists: User, pssword: string) {
+  return new Promise(function (resolve, reject) {
     try {
       const compare = bcrypt.compareSync(pssword, userExists.pssword)
       if (!compare) {
