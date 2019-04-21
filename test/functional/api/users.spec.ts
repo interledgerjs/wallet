c38e7b8baaaed58f1e8cf5ffbc99ea32a2f41a73
talpat <talonpatterson94@gmail.com>
2019-04-21 11:48:17 +0200

    testing

--- test/functional/api/users.spec.ts
+++ test/functional/api/users.spec.ts
@@ -1,35 +1,131 @@
-import { assert } from 'chai';
-import * as request from 'supertest';
-import * as app from '../../../build/app';
+import { assert } from 'chai'
+import * as request from 'supertest'
+import * as app from '../../../build/app'
 
-// .post('/user')
-describe('Test to create a new user', function () {
-  before(function () {
+// .get('/users')
+describe('Test to get all users', function () {
+  it('should return 404 status', function () {
     return request(app)
-      .get('/users/username/test_user')
+      .get('/users')
       .then(function (response) {
-        if (response.body.userName) {
-          const id = response.body.id
-          return request(app)
-            .delete('/users/' + response.body.id)
-        }
-      })
-  })
-  let data = {
-    "userName": "test_user",
-    "pssword": "123",
-    "role": "admin"
-  }
-  it('should return OK status', function () {
-    return request(app)
-      .post('/users')
-      .send(data)
-      // .set('Authorization', 'Bearer ' + token)
-      .then(function (response) {
-        assert.equal(response.status, 200)
+        assert.equal(response.status, 404)
       })
   });
 });
+
+// .post('/user')
+describe('Test to create a new user', function () {
+  describe('positive test for creating a user', function () {
+    before(function () {
+      return request(app)
+        .get('/users/username/test_user')
+        .then(function (response) {
+          if (response.body.userName) {
+            const id = response.body.id
+            return request(app)
+              .delete('/users/' + response.body.id)
+          }
+        })
+    })
+    let data = {
+      "userName": "test_user",
+      "pssword": "123",
+      "role": "admin"
+    }
+    it('should return OK status', function () {
+      return request(app)
+        .post('/users')
+        .send(data)
+        // .set('Authorization', 'Bearer ' + token)
+        .then(function (response) {
+          assert.equal(response.status, 200)
+        })
+    })
+  })
+  describe('Negative test for creating a user that already eists', function () {
+    let data = {
+      "userName": "test_user",
+      "pssword": "123",
+      "role": "admin"
+    }
+    it('should return 400 status', function () {
+      return request(app)
+        .post('/users')
+        .send(data)
+        // .set('Authorization', 'Bearer ' + token)
+        .then(function (response) {
+          assert.equal(response.status, 400)
+        })
+    })
+  })
+  describe('Negative test for creating a user with bad submitted data', function () {
+    let data = {
+      "userName": "test_user1",
+      "role": "admin"
+    }
+    it('should return 400 status', function () {
+      return request(app)
+        .post('/users')
+        .send(data)
+        // .set('Authorization', 'Bearer ' + token)
+        .then(function (response) {
+          assert.equal(response.status, 400)
+        })
+    })
+  })
+})
+
+// .post('/admin')
+describe('Test to create a new admin', function () {
+  describe('positive test for creating a admin', function () {
+    let data = {
+      "userName": "test_admin",
+      "pssword": "123",
+      "role": "admin"
+    }
+    it('should return OK status', function () {
+      return request(app)
+        .post('/admin')
+        .send(data)
+        // .set('Authorization', 'Bearer ' + token)
+        .then(function (response) {
+          assert.equal(response.status, 200)
+        })
+    })
+  })
+  describe('Negative test for creating a admin that already eists', function () {
+    let data = {
+      "userName": "test_admin",
+      "pssword": "123",
+      "role": "admin"
+    }
+    it('should return 400 status', function () {
+      return request(app)
+        .post('/admin')
+        .send(data)
+        // .set('Authorization', 'Bearer ' + token)
+        .then(function (response) {
+          assert.equal(response.status, 400)
+        })
+    })
+  })
+  describe('Negative test for creating a admin with bad submitted data', function () {
+    let data = {
+      "userName": "test_admin1",
+      "role": "admin"
+    }
+    it('should return 400 status', function () {
+      return request(app)
+        .post('/admin')
+        .send(data)
+        // .set('Authorization', 'Bearer ' + token)
+        .then(function (response) {
+          assert.equal(response.status, 400)
+        })
+    })
+  })
+})
+  
 
 // .get('/users')
 describe('Test to get all users', function () {
