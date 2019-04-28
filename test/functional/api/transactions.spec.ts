708eadda48b16e97b1e7a5281bf85f0edbf76d33
revan-wy <revan-wy@student.wethinkcode.co.za>
2019-04-28 22:18:32 +0200

    regrouped tests for .get/transactions

--- test/functional/api/transactions.spec.ts
+++ test/functional/api/transactions.spec.ts
@@ -4,6 +4,51 @@ import * as app from '../../../build/app'
 
 const database = process.env.DBNAME
 
+describe('.post/transactions', function () {
+  it('should return HTTP 200 when called with good data', function () {
+    let data = {
+      'debitAccount': 1,
+      'creditAccount': 2,
+      'amount': 100
+    }
+    return request(app)
+      .post('/transactions')
+      .send(data)
+      .then(function (response) {
+        assert.equal(response.status, 200)
+      })
+  })
+
+  it('should return HTTP 400 when called with bad data', function () {
+    let data = {
+      'debitAccount': 1,
+      'creditAccount': 2,
+      'amount': 'asd'
+    }
+    return request(app)
+      .post('/transactions')
+      .send(data)
+      .then(function (response) {
+        assert.equal(response.status, 400)
+      })
+  })
+
+  it('should return HTTP 500 when db cannot be found', function () {
+    process.env.DBNAME = ''
+    let data = {
+      'debitAccount' : 1,
+      'creditAccount' : 2,
+      'amount' : 100
+    }
+    return request(app)
+      .post('/transactions')
+      .send(data)
+      .then(function (response) {
+        assert.equal(response.status, 500)
+      })
+  })
+})
+
 // .get('/transactions')
 describe('Test to get all transactions but return 404 due to no transactions', function () {
   it('1. should return HTTP 404 when db table is empty', function () {
@@ -17,52 +62,6 @@ describe('Test to get all transactions but return 404 due to no transactions', f
 
 // .post('/transaction')
 describe('Test to create a new transaction', function () {
-  describe('Positive test to create a new transaction', function () {
-    let data = {
-      'debitAccount': 1,
-      'creditAccount': 2,
-      'amount': 100
-    }
-    it('2. should return HTTP 200 when called with good data', function () {
-      return request(app)
-        .post('/transactions')
-        .send(data)
-        .then(function (response) {
-          assert.equal(response.status, 200)
-        })
-    })
-  })
-  describe('Negative test to create a transaction with invalid data', function () {
-    let data = {
-      'debitAccount': 1,
-      'creditAccount': 2,
-      'amount': 'asd'
-    }
-    it('3. should return HTTP 400 when called with bad data', function () {
-      return request(app)
-        .post('/transactions')
-        .send(data)
-        .then(function (response) {
-          assert.equal(response.status, 400)
-        })
-    })
-  })
-  describe('Test to check if there is an error with the data layer', function () {
-    let data = {
-      'debitAccount' : 1,
-      'creditAccount' : 2,
-      'amount' : 100
-    }
-    it('4. should return HTTP 500 when db cannot be found', function () {
-      process.env.DBNAME = ''
-      return request(app)
-        .post('/transactions')
-        .send(data)
-        .then(function (response) {
-          assert.equal(response.status, 500)
-        })
-    })
-  })
   afterEach(function () {
     process.env.DBNAME = database
   })
