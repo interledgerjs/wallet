7bbf76cc891a296e5562db795486a72d87baf969
talpat <talonpatterson94@gmail.com>
2019-05-09 14:26:43 +0200

    circle troubleshooting

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,7 @@ const databaseName = process.env.DBNAME || process.env.MYSQL_DB
 const databaseUser = process.env.DBUSER || process.env.MYSQL_USER
 
 console.log("db pwd: " + databasePassword)
+console.log("are paswords allowed: " + process.env.MYSQL_ALLOW_EMPTY_PASSWORD)
 
 module.exports = {
   client: 'mysql',
