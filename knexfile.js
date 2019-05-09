0cd2ac5bb0543f78a07b66e84f3b66d12f01310f
talpat <talonpatterson94@gmail.com>
2019-05-09 15:07:01 +0200

    circle troubleshooting

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,11 @@
 const dotenv = require('dotenv').config()
 
-const databasePassword = process.env.DBPASSWORD || process.env.MYSQL_PASSWORD
+const databasePassword = process.env.DBPASSWORD
 const databaseName = process.env.DBNAME || process.env.MYSQL_DB
 const databaseUser = process.env.DBUSER || process.env.MYSQL_USER
 
 console.log("db pwd: " + databasePassword)
-console.log("are paswords allowed: " + process.env.MYSQL_ALLOW_EMPTY_PASSWORD)
+console.log("are empty passwords allowed: " + process.env.MYSQL_ALLOW_EMPTY_PASSWORD)
 
 module.exports = {
   client: 'mysql',
