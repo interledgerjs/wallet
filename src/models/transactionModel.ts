96acda24d97f607b53060e3f86c376934444ba85
revan-wy <revan-wy@student.wethinkcode.co.za>
2019-05-09 18:31:14 +0200

    removed double promises

--- src/models/transactionModel.ts
+++ src/models/transactionModel.ts
@@ -31,84 +31,58 @@ export function isTransactionArray (result: any): result is Transaction[] {
 }
 
 // function to handle adding transactions
-export function addTransaction (body: any): Promise<boolean> {
-  return new Promise(async function (resolve: any, reject) {
-    try {
-      if (body) {
-        let result = await knexInsert(body, 'transactions')
-        resolve(false)
-      } else {
-        resolve(true)
-      }
-    } catch (error) {
-      reject(error)
-    }
-  })
+export async function addTransaction (body: any): Promise<boolean> {
+  if (body) {
+    let result = await knexInsert(body, 'transactions')
+    return (false)
+  } else {
+    return (true)
+  }
 }
 
 // function to handle getting all transactions
-export function retrieveTransactions (): Promise<Transaction[]> {
-  return new Promise(async function (resolve, reject) {
-    try {
-      let result = await knexSelectAll('transactions')
-      if (isTransactionArray(result)) {
-        resolve(result)
-      } else {
-        reject('Not transaction array')
-      }
-    } catch (error) {
-      reject(error)
-    }
-  })
+export async function retrieveTransactions (): Promise<Transaction[]> {
+  let result = await knexSelectAll('transactions')
+  if (isTransactionArray(result)) {
+    return (result)
+  } else {
+    throw new Error('Not transaction array')
+  }
 }
 
 // function to handle getting transactions by id
-export function retrieveTransactionById (id: number): Promise<Transaction> {
-  return new Promise(async function (resolve, reject) {
-    try {
-      let result = await knexSelectById(id, 'transactions')
-      if (isTransactionArray(result)) {
-        if (result.length > 0) {
-          resolve(result[0])
-        } else {
-          resolve(null)
-        }
-      } else {
-        reject('Not transaction array')
-      }
-    } catch (error) {
-      reject(error)
+export async function retrieveTransactionById (id: number): Promise<Transaction> {
+  let result = await knexSelectById(id, 'transactions')
+  if (isTransactionArray(result)) {
+    if (result.length > 0) {
+      return (result[0])
+    } else {
+      return (null)
     }
-  })
+  } else {
+    throw new Error('Not transaction array')
+  }
 }
 
 // function to handle getting transactions by account id
-export function retrieveTransactionsByAccountId (id: number): Promise<Transaction[]> {
-  return new Promise(async function (resolve, reject) {
-    try {
-      let result = await knexSelectTransactionByEitherAccount(id, 'transactions')
-      if (isTransactionArray(result)) {
-        resolve(result)
-      } else {
-        reject('Not transaction array')
-      }
-    } catch (error) {
-      reject(error)
-    }
-  })
+export async function retrieveTransactionsByAccountId (id: number): Promise<Transaction[]> {
+  let result = await knexSelectTransactionByEitherAccount(id, 'transactions')
+  if (isTransactionArray(result)) {
+    return (result)
+  } else {
+    throw new Error('Not transaction array')
+  }
 }
 
-function buildTransaction (body: any, baseObj: Transaction = undefined): Promise<Transaction> {
-  return new Promise(async function (resolve, reject) {
-    if (baseObj === undefined) {
-      const transactionObject: Transaction = {
-        id: -1,
-        debitAccountId: body.debitAccountId,
-        creditAccountId: body.creditAccountId,
-        amount: body.amount,
-        date: new Date().toISOString()
-      }
-      resolve(transactionObject)
+function buildTransaction (body: any, baseObj: Transaction = undefined): Transaction {
+  if (baseObj === undefined) {
+    const transactionObject: Transaction = {
+      id: -1,
+      debitAccountId: body.debitAccountId,
+      creditAccountId: body.creditAccountId,
+      amount: body.amount,
+      date: new Date().toISOString()
     }
-  })
+    return (transactionObject)
+  }
 }
--- src/services/tokenService.ts
+++ src/services/tokenService.ts
@@ -2,16 +2,10 @@ import * as bcrypt from 'bcrypt'
 import { User } from '../models'
 
 export function compareHash (userExists: User, pssword: string) {
-  return new Promise(function (resolve, reject) {
-    try {
-      const compare = bcrypt.compareSync(pssword, userExists.pssword)
-      if (!compare) {
-        resolve(false)
-      } else {
-        resolve(true)
-      }
-    } catch (error) {
-      reject(error)
-    }
-  })
+  const compare = bcrypt.compareSync(pssword, userExists.pssword)
+  if (!compare) {
+    return (false)
+  } else {
+    return (true)
+  }
 }
