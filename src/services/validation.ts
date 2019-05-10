e9e82236cc996085d4f898f014032554eb58e8a0
jadinek <36467054+jadinek@users.noreply.github.com>
2019-05-10 13:41:00 +0200

    Update validation.ts

--- src/services/validation.ts
+++ src/services/validation.ts
@@ -35,7 +35,7 @@ export function validate (req, res) {
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
     // res.status(422).json({ errors: errors.array() })
-    res.send(400)
+    res.sendStatus(400)
     return false
   } else {
     return true
