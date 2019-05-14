4bacbe498b11dd65d4472d324e15eb617bd38a6c
Renzo van Wyk <40379554+revan-wy@users.noreply.github.com>
2019-05-14 12:52:07 +0200

    Added new database seeds (#243)
    * removed package typedoc
    
    * removed fn query from dbService.spec
    
    * added redb script
    
    * expanded users seeds
    
    * cleaned users seeds
    
    * expanded accounts seeds
    
    * added transactions seeds

--- database/seeds/01accounts.js
+++ database/seeds/01accounts.js
@@ -3,11 +3,41 @@ const dotenv = require('dotenv').config()
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('accounts').del()
+  // Inserts seed entries
   .then(async function () {
-    // Inserts seed entries
     return knex('accounts').insert({
       name: 'Equity',
       owner: 1
+    });
+  })
+  .then(async function () {
+    return knex('accounts').insert({
+      name: 'home',
+      owner: 2
+    });
+  })
+  .then(async function () {
+    return knex('accounts').insert({
+      name: 'cash',
+      owner: 2
+    });
+  })
+  .then(async function () {
+    return knex('accounts').insert({
+      name: 'fuel',
+      owner: 3
+    });
+  })
+  .then(async function () {
+    return knex('accounts').insert({
+      name: 'food',
+      owner: 3
+    });
+  })
+  .then(async function () {
+    return knex('accounts').insert({
+      name: 'beverages',
+      owner: 4
     });
   });
 };
