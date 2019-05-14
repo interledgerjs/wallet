fcf35879851356bb491203f74f70aef042d5487f
talpat <talonpatterson94@gmail.com>
2019-05-14 17:40:38 +0200

    filter deleted bug fix

--- src/services/filters.ts
+++ src/services/filters.ts
@@ -3,11 +3,13 @@ import { User, Account } from '../models'
 export function filterDeleted (objectArray: User[] | Account[]): Promise<User[] | Account[]> {
   return new Promise(async function (resolve, reject) {
     let returnArray: User[] | Account[] = []
-    objectArray.forEach(element => {
-      if (!element.deletedAt) {
-        returnArray.push(element)
-      }
-    })
+    if (objectArray) {
+      objectArray.forEach(element => {
+        if (!element.deletedAt) {
+          returnArray.push(element)
+        }
+      })
+    }
     resolve(returnArray)
   })
 }
