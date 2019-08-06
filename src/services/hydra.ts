42c8459ef560da363cc9e79ddb763bf8c91eb6f2
Matthew de Haast <matt.dehaast@coil.com>
2019-08-06 12:07:20 +0200

    Add oauth apis (#254)
    * fix secret
    
    * feat(oauth):
    * Begin adding oauth apis to the wallet
    
    * feat(oauth):
    * Change agreement to get from API

---
+++ src/services/hydra.ts
@@ -0,0 +1,80 @@
+import axios from 'axios'
+const querystring = require('querystring')
+
+const hydraUrl = process.env.HYDRA_ADMIN_URL || 'http://localhost:9001'
+let mockTlsTermination = {}
+process.env.MOCK_TLS_TERMINATION = 'true'
+if (process.env.MOCK_TLS_TERMINATION) {
+  mockTlsTermination = {
+    'X-Forwarded-Proto': 'https'
+  }
+}
+
+type Flow = 'login' | 'consent' | 'logout'
+type Action = 'accept' | 'reject'
+
+// A little helper that takes type (can be "login" or "consent") and a challenge and returns the response from ORY Hydra.
+function get (flow: Flow, challenge: string) {
+  const url = new URL('/oauth2/auth/requests/' + flow, hydraUrl)
+  url.search = querystring.stringify({ [flow + '_challenge']: challenge })
+  return axios.get(url.toString(), {
+    headers: {
+      'X-Forwarded-Proto': 'https'
+    },
+    timeout: 5000
+  }).then(res => {
+    return res.data
+  })
+}
+
+// A little helper that takes type (can be "login" or "consent"), the action (can be "accept" or "reject") and a challenge and returns the response from ORY Hydra.
+function put (flow: Flow, action: Action, challenge: string, body: any) {
+  const url = new URL('/oauth2/auth/requests/' + flow + '/' + action, hydraUrl)
+  url.search = querystring.stringify({ [flow + '_challenge']: challenge })
+  return axios.put(url.toString(), body, {
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Forwarded-Proto': 'https'
+    },
+    timeout: 5000
+  }).then(res => res.data)
+}
+
+export const hydra = {
+  // Fetches information on a login request.
+  getLoginRequest: function (challenge: string) {
+    return get('login', challenge)
+  },
+  // Accepts a login request.
+  acceptLoginRequest: function (challenge: string, body: any) {
+    return put('login', 'accept', challenge, body)
+  },
+  // Rejects a login request.
+  rejectLoginRequest: function (challenge: string, body: any) {
+    return put('login', 'reject', challenge, body)
+  },
+  // Fetches information on a consent request.
+  getConsentRequest: function (challenge: string) {
+    return get('consent', challenge)
+  },
+  // Accepts a consent request.
+  acceptConsentRequest: function (challenge: string, body: any) {
+    return put('consent', 'accept', challenge, body)
+  },
+  // Rejects a consent request.
+  rejectConsentRequest: function (challenge: string, body: any) {
+    return put('consent', 'reject', challenge, body)
+  },
+  // Fetches information on a logout request.
+  getLogoutRequest: function (challenge: string) {
+    return get('logout', challenge)
+  },
+  // Accepts a logout request.
+  acceptLogoutRequest: function (challenge: string) {
+    return put('logout', 'accept', challenge, {})
+  },
+  // Reject a logout request.
+  rejectLogoutRequest: function (challenge: string) {
+    return put('logout', 'reject', challenge, {})
+  }
+}
